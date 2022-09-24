40c96c69787d200ca7128d4ae0482e854a82c8ab
Yi-Ru Lan <a78945612385238@gmail.com>
2022-09-25 01:22:13 +0800

    Using declare as a last resort

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,11 @@ import '@/main.css';
 import _ from 'lodash';
 
 console.log(_.shuffle([1, 2, 3]));
+
+// declare var Global_Variable: string;
+// declare let Local_Variable: string;
+// declare const ReadOnly_Variable = 30;
+
+// console.log(Global_Variable);
+// console.log(Local_Variable);
+// console.log(ReadOnly_Variable);
