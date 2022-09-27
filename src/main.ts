0b8647aaf7546b7ed93cdb5eb47d88860c27f99b
Yi-Ru Lan <a78945612385238@gmail.com>
2022-09-27 17:26:53 +0800

    No Types Needed: class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import '@/main.css';
 
 import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToInstance } from 'class-transformer';
+import Product from '@/product.model';
 
 console.log(_.shuffle([1, 2, 3]));
 
@@ -11,3 +14,21 @@ console.log(_.shuffle([1, 2, 3]));
 // console.log(Global_Variable);
 // console.log(Local_Variable);
 // console.log(ReadOnly_Variable);
+
+const products = [
+  { title: 'A carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+];
+
+// const p1 = new Product('A Book', 12.99);
+// console.log(p1.getInformation());
+
+// const loadedProducts = products.map((product) => {
+//   return new Product(product.title, product.price);
+// });
+
+const loadedProducts = plainToInstance(Product, products);
+
+loadedProducts.forEach((product) => {
+  console.log(product.getInformation());
+});
---
+++ src/product.model.ts
@@ -0,0 +1,7 @@
+export default class Product {
+  constructor(public title: string, public price: number) {}
+
+  getInformation() {
+    return [this.title, `$${this.price}`];
+  }
+}
